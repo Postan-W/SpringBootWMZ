912986082ad486710f89fddb089943fb3c2cc1a8
mingzhu <15216802238@163.com>
2020-12-07 11:54:08 +0800

    2020-12-7

---
+++ src/main/java/com/mingzhu/spring/AnnotattionTag.java
@@ -0,0 +1,9 @@
+package com.mingzhu.spring;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnotattionTag {
+
+}
